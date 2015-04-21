c9fbf19b5762337c3268b797ca8d4ec264f69e84
kriahja <kriahja@gmail.com>
2015-04-21 15:05:58 +0200

    BLL Text

---
+++ src/BLL/TextManager.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BLL;
+
+import BLL.Exceptions.BivExceptions;
+import Entities.Text;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author notandi
+ */
+public class TextManager
+{
+
+    private static TextManager instance = null;
+
+    private static TextDBManager db;
+    private List<Text> cars;
+
+    private TextManager()
+    {
+        cars = new ArrayList<>();
+        try
+        {
+            db = TextDBManager.getInstance();
+        }
+        catch (IOException ex)
+        {
+            throw new BivExceptions("Unable to connect to Text database");
+        }
+    }
+
+    public static TextManager getInstance()
+    {
+        if (instance == null)
+        {
+            instance = new TextManager();
+        }
+        return instance;
+    }
+
+    public ArrayList<Text> readAll()
+    {
+        return db.readAll();
+    }
+    
+    public Text getById(int id)
+    {
+        return db.readById(id);
+    }
+
+    public ArrayList<Text> getByPriorityId(int priId)
+    {
+        return db.readByPriorityId(priId);
+    }
+
+    public ArrayList<Text> getByDisplayId(int dispId)
+    {
+        return db.readByDisplayId(dispId);
+    }
+
+    public ArrayList<Text> getBySafe(boolean safe)
+    {
+        return db.readBySafe(safe);
+    }
+
+    public Text getByTitle(String title)
+    {
+        return db.readByTitle();
+    }
+
+    /**
+     * getByid(int id) getByPriorityId (int priid) getByDisplayId (int dispId)
+     * getByTimer (String getByStartdate (Date getByEndDate (Date getByNotSafe
+     * (Boolean getByText (String getByTitle (String
+     */
+}
