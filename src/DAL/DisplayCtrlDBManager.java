e2bb4487e06f4acce0d8622eca239c85270b30ce
a.tamas <a.tamas@10.176.165.159>
2015-05-18 15:10:30 +0200

    boooyea

--- src/DAL/DisplayCtrlDBManager.java
+++ src/DAL/DisplayCtrlDBManager.java
@@ -47,34 +47,35 @@ public class DisplayCtrlDBManager
      * @param rs results of the query.
      * @return new Text
      */
-    private Text getOneText(ResultSet rs) throws SQLException
-    {
-        int id = rs.getInt("ID");
-        int presTypeId = rs.getInt("PresTypeId");
-        String title = rs.getString("Title");
-
-        Date startDate = rs.getDate("StartDate");
-        Date endDate = rs.getDate("EndDate");
-        Double timer = rs.getDouble("Timer");
-
-        boolean notSafe = rs.getBoolean("NotSafe");
-        boolean disable = rs.getBoolean("Disable");
-
-        String PresName = rs.getString("ScreenName");
-
-//        String depName = rs.getString("Name");
-        return new Text(id, presTypeId, title, startDate, endDate, timer, notSafe);
-
-    }
-
+//    private Text getOneText(ResultSet rs) throws SQLException
+//    {
+//        int id = rs.getInt("ID");
+//        int presTypeId = rs.getInt("PresTypeId");
+//        String title = rs.getString("Title");
+//
+//        Date startDate = rs.getDate("StartDate");
+//        Date endDate = rs.getDate("EndDate");
+//        Double timer = rs.getDouble("Timer");
+//
+//        boolean notSafe = rs.getBoolean("NotSafe");
+//        boolean disable = rs.getBoolean("Disable");
+//
+//        String PresName = rs.getString("ScreenName");
+//
+////        String depName = rs.getString("Name");
+//        return new Text(id, presTypeId, title, startDate, endDate, timer, notSafe);
+//
+//    }
     public DisplayCtrl getOnedc(ResultSet rs) throws SQLException
     {
         String presTitle = rs.getString("Title");
         String presType = rs.getString("Type");
         String screenName = rs.getString("ScreenName");
         boolean disable = rs.getBoolean("Disable");
+        int presId = rs.getInt("PresentationId");
+        int dispId = rs.getInt("DisplayId");
 
-        DisplayCtrl dc = new DisplayCtrl(presTitle, presType, screenName, disable);
+        DisplayCtrl dc = new DisplayCtrl(presTitle, presType, screenName, disable, presId, dispId);
 
         return dc;
     }
@@ -83,11 +84,11 @@ public class DisplayCtrlDBManager
     {
         try (Connection con = cm.getConnection()) {
             ArrayList<DisplayCtrl> dcList = new ArrayList<>();
-            String sql = "Select Presentation.Title, PresType.[Type], DisplayCtrl.[Disable], Display.ScreenName \n"
-                    + "       from DisplayCtrl\n"
-                    + "       Inner JOIN Presentation ON DisplayCtrl.PresentationId = Presentation.ID\n"
-                    + "       Inner JOIN PresType ON Presentation.PresTypeId = PresType.ID\n"
-                    + "       Inner JOIN Display ON DisplayCtrl.DisplayId = Display.ID";
+            String sql = "Select Presentation.Title, PresType.[Type], DisplayCtrl.[Disable], Display.ScreenName , DisplayCtrl.PresentationId , DisplayCtrl.DisplayId\n"
+                    + "                    from DisplayCtrl\n"
+                    + "                    Inner JOIN Presentation ON DisplayCtrl.PresentationId = Presentation.ID\n"
+                    + "                    Inner JOIN PresType ON Presentation.PresTypeId = PresType.ID\n"
+                    + "                    Inner JOIN Display ON DisplayCtrl.DisplayId = Display.ID";
             Statement st = con.createStatement();
             ResultSet rs = st.executeQuery(sql);
 
@@ -105,20 +106,20 @@ public class DisplayCtrlDBManager
      */
     public void updateDisable(DisplayCtrl dc) throws SQLException
     {
-        try (Connection con = cm.getConnection())
-        {
-            String sql = "update DisplayCtrl set Disable = ? where PresentationId = ?";
+        try (Connection con = cm.getConnection()) {
+            System.out.println(dc.isDisable() + "" + dc.getPresId());
+            String sql = "update DisplayCtrl set Disable = ? where PresentationId = ? and DisplayId = ?";
             PreparedStatement ps = con.prepareStatement(sql);
 
             ps.setBoolean(1, dc.isDisable());
             ps.setInt(2, dc.getPresId());
+            ps.setInt(3, dc.getDispId());
 
             int affectedRows = ps.executeUpdate();
-            if (affectedRows == 0)
-            {
+            if (affectedRows == 0) {
                 throw new BivExceptions("Unable to Update Dispctrl.");
             }
 
         }
-    }    
+    }
 }
